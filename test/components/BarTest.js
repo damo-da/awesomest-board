e460c364c649564be6b62beeb6af30f09bcb8f33
Damodar Dahal <underscoredam@gmail.com>
2017-05-02 12:57:32 -0500

    rename test to correct file name

--- test/components/BarTest.js
+++ test/components/BarTest.js
@@ -3,18 +3,19 @@
 /* eslint no-console: 0 */
 'use strict';
 
-// Uncomment the following lines to use the react test utilities
-// import TestUtils from 'react-addons-test-utils';
 import createComponent from 'helpers/shallowRenderHelper';
 
-describe('MainPageComponent', () => {
+import { Bar } from "components/Bar";
+
+describe('Bar', () => {
   let component;
 
-  // beforeEach(() => {
-  //   component = createComponent(MainPageComponent);
-  // });
+  beforeEach(() => {
+    component = createComponent(Bar);
+  });
 
-  // it('should have its component name as default className', () => {
-  //   expect(component.props.className).to.equal('mainpage-component');
-  // });
+  it('should have its component name as default className', () => {
+    expect(component.props.className).to.equal('bar-component');
+  });
+
 });
