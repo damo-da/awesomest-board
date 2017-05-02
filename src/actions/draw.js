305a2a00489690abe4f2958777ed2fcfb4ec96a7
Damodar Dahal <underscoredam@gmail.com>
2017-05-02 16:55:46 -0500

    save drawing state locally

--- src/actions/draw.js
+++ src/actions/draw.js
@@ -4,6 +4,7 @@ let canvas = null;
 let ctx = null;
 
 window.lastEvent = {}; // this stores the data for the last event by userId
+const events = [];
 
 export const initCanvas = (c, w, h) => {
   canvas = c;
@@ -13,33 +14,36 @@ export const initCanvas = (c, w, h) => {
   canvas.width = w;
   canvas.height = h;
 
-  canvas.style.width = w+'px';
-  canvas.style.height = h+'px';
+  canvas.style.width = w + 'px';
+  canvas.style.height = h + 'px';
 
   ctx.fillStyle = 'rgb(200,200,200)';
   ctx.fillRect(0, 0, w, h);
 
   ctx.fillStyle = 'rgb(200,500,200)';
+
+  events.forEach(event => draw(...event));
 };
 
-const draw = (x, y, pencilId='DEFAULT', size=10, color='#0000ff', type='', userId=0) => {
+const draw = (x, y, pencilId = 'DEFAULT', size = 10, color = '#0000ff', type = '', userId = 0) => {
   const pencil = getPencilById(pencilId);
 
-  const event = {x, y, size, color, type, lastEvent: window.lastEvent[userId]};
+  const event = {x: x* canvas.width, y: y * canvas.height, size, color, type, lastEvent: window.lastEvent[userId]};
 
   window.lastEvent[userId] = event;
 
   pencil.draw(ctx, event);
 };
 
-export const runEvent = (action, x, y, pencilData, userId=0) => {
-  draw(x * canvas.width, y*canvas.height, pencilData.pencil, pencilData.size, pencilData.color, action, userId);
-
+export const runEvent = (action, x, y, pencilData, userId = 0) => {
+  const args = [x, y, pencilData.pencil, pencilData.size, pencilData.color, action, userId]
+  events.push(args);
+  draw(...args);
 };
 
 
 export const clearBoard = () => {
-  if (canvas){
+  if (canvas) {
     initCanvas(canvas, canvas.width, canvas.height);
   }
 };
@@ -47,7 +51,7 @@ export const clearBoard = () => {
 export const saveCanvas = () => {
   canvas.toDataURL('image/png');
   const drawingString = canvas.toDataURL('png');
-  let a  = document.createElement('a');
+  let a = document.createElement('a');
   a.href = drawingString;
   a.download = 'canvas.png';
   a.click();
